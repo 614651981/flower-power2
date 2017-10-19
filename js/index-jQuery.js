18db7472ae27ff00028e12beded97baf4d51dfd4
Your Name <614651981@qq.com>
2017-10-19 14:00:20 +0800

    'flower'

---
+++ js/index-jQuery.js
@@ -0,0 +1,84 @@
+$(function(){
+
+ 	//鼠标滚动导航的固定与消失
+ 	  
+ 	var navOffset=$("header").offset().top;  
+	    $(window).scroll(function(){  
+	        var scrollPos=$(window).scrollTop();  
+	        if(scrollPos >navOffset){
+	            $("header").addClass("fixed");  
+	        }else{  
+	            $("header").removeClass("fixed");
+	        }  
+    }); 
+
+    var navOffset=$(".head").offset().top;  
+	    $(window).scroll(function(){  
+	        var scrollPos=$(window).scrollTop();  
+	        if(scrollPos >=navOffset){  
+	            $(".head").addClass("fixed");  
+	        }else{  
+	            $(".head").removeClass("fixed");  
+	        }  
+    });  
+
+
+
+ 	//侧导航
+	$(".iconfont1").click(function() {
+		$(".section-aside").css("display","block");
+	});
+	$(".section-aside").dblclick(function() {
+		$(this).css("display","none");
+	});
+
+
+	//banner图轮播
+ 	function rollOne(){  
+	    $(".big-img >ul").animate({marginLeft:"-1519px"},2000,"linear",function(){
+	        $(this).css({marginLeft:"0px"});  
+	        $(this).children("li").first().remove().clone(true).appendTo(".big-img >ul");  
+	    });  
+    }  
+	    var startRollOne=setInterval(rollOne,2000);  
+	    $(".big-img >ul").hover(function () {
+	        clearInterval(startRollOne);  
+	    }, function () {  
+	        startRollOne=setInterval(rollOne,2000);  
+	    });
+
+	//商城--> 花、自动轮播
+
+	function rolltwo(){
+        let imgw=$('.products').find('img').width();
+        console.log(imgw);
+    $(".hualist-ul").animate({marginLeft:`-${imgw}px`},2000,"linear",function(){
+        $(this).css({marginLeft:"0px"});
+        $(this).children("li").first().remove().clone(true).appendTo(".hualist-ul");
+    });
+	}
+	var startRolltwo=setInterval(rolltwo,2000);
+	$(".hualist-ul").hover(function () {
+		clearInterval(startRolltwo);
+	}, function () {
+		startRolltwo=setInterval(rolltwo,2000);
+	});
+
+//最新优惠
+	let lis=$(".products");
+	let zhezhaos=$(".zhezhao");
+
+	for(let i=0;i<lis.length;i++){
+		lis.eq(i).mousemove(function(){
+		zhezhaos.eq(i).css("display","block");
+
+	});
+		lis.eq(i).mouseout(function(){
+			zhezhaos.eq(i).css("display","none");
+		})
+	}
+	
+
+
+
+});
